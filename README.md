85be846733c1adf57d8feb5e3313a5d6088ce375
Steven Carter <steve@javapl.us>
2026-07-17 16:15:03 -0400

    Optimized day 8 by removing lots of Vec creation

--- README.md
+++ README.md
@@ -15,12 +15,12 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 | [Day 2](./src/bin/02.rs) | `9.8µs` | `298.0µs` |
 | [Day 3](./src/bin/03.rs) | `502.9µs` | `151.0µs` |
 | [Day 7](./src/bin/07.rs) | `21.1µs` | `27.2µs` |
-| [Day 8](./src/bin/08.rs) | `324.5µs` | `302.0µs` |
+| [Day 8](./src/bin/08.rs) | `98.1µs` | `128.6µs` |
 | [Day 9](./src/bin/09.rs) | `558.7µs` | `56.0ms` |
 | [Day 10](./src/bin/10.rs) | `82.8µs` | `77.9µs` |
 | [Day 11](./src/bin/11.rs) | `1.9ms` | `13.3ms` |
 | [Day 12](./src/bin/12.rs) | `104.1µs` | `7.4ms` |
 | [Day 13](./src/bin/13.rs) | `826.4µs` | `2.1ms` |
 
-**Total: 87.30ms**
+**Total: 86.90ms**
 <!--- benchmarking table --->
--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,102 +1,39 @@
 advent_of_code::solution!(8);
 
-pub struct Node {
-    pub child_node_count: u8,
-    pub metadata_count: u8,
-    pub child_nodes: Vec<Node>,
-    pub metadata_entries: Vec<u8>,
+fn parse(input: &str) -> impl Iterator<Item = u32> + '_ {
+    input.split_ascii_whitespace().map(|n| n.parse().unwrap())
 }
 
-impl Node {
-    pub fn parse(input: &[u8]) -> (Option<Self>, &[u8]) {
-        if input.is_empty() {
-            return (None, input);
-        }
-        let child_node_count = input[0];
-        let metadata_count = input[1];
-        let mut child_nodes = Vec::new();
-        let mut metadata_entries = Vec::new();
+fn metadata_sum(it: &mut impl Iterator<Item = u32>) -> u32 {
+    let children = it.next().unwrap();
+    let metas = it.next().unwrap();
+    let child_sum: u32 = (0..children).map(|_| metadata_sum(it)).sum();
+    let meta_sum: u32 = (0..metas).map(|_| it.next().unwrap()).sum();
+    child_sum + meta_sum
+}
 
-        let mut remainder = &input[2..];
-
-        if child_node_count > 0 {
-            for _ in 0..child_node_count {
-                let (node, new_remainder) = Node::parse(remainder);
-                child_nodes.push(node.unwrap());
-                remainder = new_remainder;
+fn node_value(it: &mut impl Iterator<Item = u32>) -> u32 {
+    let children = it.next().unwrap() as usize;
+    let metas = it.next().unwrap();
+    let child_values: Vec<u32> = (0..children).map(|_| node_value(it)).collect();
+    (0..metas)
+        .map(|_| it.next().unwrap() as usize)
+        .map(|m| {
+            if children == 0 {
+                m as u32
+            } else {
+                child_values.get(m.wrapping_sub(1)).copied().unwrap_or(0)
             }
-        }
-
-        if metadata_count > 0 {
-            metadata_entries = remainder[0..metadata_count as usize].to_vec();
-            remainder = &remainder[metadata_count as usize..];
-        }
-
-        (
-            Some(Self {
-                child_node_count,
-                metadata_count,
-                child_nodes,
-                metadata_entries,
-            }),
-            remainder,
-        )
-    }
-
-    pub fn metadata_sum(&self) -> u32 {
-        self.metadata_entries.iter().map(|&n| n as u32).sum::<u32>()
-            + self
-                .child_nodes
-                .iter()
-                .map(|cn| cn.metadata_sum())
-                .sum::<u32>()
-    }
-
-    pub fn node_value(&self) -> u32 {
-        if self.child_nodes.is_empty() {
-            self.metadata_entries.iter().map(|&n| n as u32).sum::<u32>()
-        } else {
-            self.metadata_entries
-                .iter()
-                .filter_map(|&m| {
-                    if m == 0 {
-                        None
-                    } else {
-                        self.child_nodes.get((m - 1) as usize)
-                    }
-                })
-                .map(|n| n.node_value())
-                .sum()
-        }
-    }
+        })
+        .sum()
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let data: Vec<u8> = input
-        .trim()
-        .split(' ')
-        .map(|ch| {
-            ch.parse::<u8>()
-                .unwrap_or_else(|_| panic!("could not convert '{ch}'"))
-        })
-        .collect();
-    let (top_node, _) = Node::parse(&data);
-
-    Some(top_node.unwrap().metadata_sum())
+    Some(metadata_sum(&mut parse(input)))
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let data: Vec<u8> = input
-        .trim()
-        .split(' ')
-        .map(|ch| {
-            ch.parse::<u8>()
-                .unwrap_or_else(|_| panic!("could not convert '{ch}'"))
-        })
-        .collect();
-    let (top_node, _) = Node::parse(&data);
-
-    Some(top_node.unwrap().node_value())
+    Some(node_value(&mut parse(input)))
 }
 
 #[cfg(test)]
